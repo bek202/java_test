c23d7e76051b7ef56c7e1558fcb62165bdf1ed92
Azizbek Begmurodov <bekmurodov.a@mail.ru>
2022-03-17 11:41:02 +0500

    Solution for 2nd task

--- src/test2.java
+++ src/test2.java
@@ -1,6 +1,18 @@
 public class test2 {
+    static int f(int[] a){
+        int x, y;
+        x = y = 0;
+        for (int i = 0; i < a.length; i++){
+            if (a[i] % 2 == 0)
+                y = y + a[i];
+            else
+                x = x + a[i];
+        }
+        return x-y;
+    }
 
     public static void main(String[] args){
-        System.out.println("Yeah");
+        int[] arr = {1,2,3,4,5};
+        System.out.println(f(arr));
     }
 }
